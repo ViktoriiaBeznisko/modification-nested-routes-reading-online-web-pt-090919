0e5de3962a6595bc51a1488b636312ca98d960ed
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-03-02 02:52:51 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,30 +15,42 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
     end
   end
-
+   
   def new
-    @post = Post.new
+    @post = Post.new(author_id: params[:author_id])
   end
+
 
   def create
     @post = Post.new(post_params)
     @post.save
     redirect_to post_path(@post)
   end
+  
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to post_path(@post)
+end
+  
 
-  def update
+def edit
+  if params[:author_id]
+    author = Author.find_by(id: params[:author_id])
+    if author.nil?
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = author.posts.find_by(id: params[:id])
+      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+    end
+  else
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
   end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
+end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
